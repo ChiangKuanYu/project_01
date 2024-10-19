163ab25e49db93fb231b350049415fc3e2287f03
Smoger <erinkenken@hotmail.com>
2024-10-19 20:32:47 +0800

    add mapbox delete token

--- src/components/MapContainer/Mapbox.jsx
+++ src/components/MapContainer/Mapbox.jsx
@@ -11,8 +11,7 @@ import "./Mapbox.css";
 import TMountain from "../../assets/Json/taiwanMountain.json";
 
 // const TOKEN = process.env.REACT_APP_TOKEN;
-const TOKEN =
-  "pk.eyJ1Ijoia3kwODA5IiwiYSI6ImNsemo5bHM1bjBhdmcya29tc3d2NmlxdHMifQ.6rDng0xIZd2-FOvPc1jddA";
+const TOKEN = "your mapbox API";
 function Mapbox() {
   const [viewPort, setViewPort] = useState({
     longitude: 120.751864,
